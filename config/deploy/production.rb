8491df3f06168d7f4dfabf896977457828cfec12
ryfon <1157094+ryfon@users.noreply.github.com>
2018-05-22 17:47:00 -0400

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :user, "ryfon"
 set :rails_env, "production"
-server "192.168.1.105", :roles => %w(web app db), :primary => true, :user => "ryfon"
+server "pic.ryfon.com", :roles => %w(web app db), :primary => true, :user => "ryfon"
 #server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
